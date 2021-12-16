b631e7a32c52609509bff7b2280dd5443ec1587e
Евкакий <buligani@mail.ru>
2021-12-16 22:36:22 +0300

    86

--- source/components/posts.js
+++ source/components/posts.js
@@ -3,15 +3,21 @@ import { apiService } from "../services/api.js";
 import { TransformService } from "../services/transform.js";
 
 export class PostsComponent extends Component {
-  constructor(id) {
-    super(id)
+  constructor(id, {loader}) {
+    super(id),
+    this.loader = loader;
   };
 
+  init() {
+    this.el.addEventListener('click', buttonHandler.bind(this));
+  }
+
   async onshow () {
+    this.loader.show();
     const fbData = await apiService.fetchPosts();
     const posts = TransformService.fbObjectToArray(fbData);
     const html = posts.map(post => renderPost(post));
-
+    this.loader.hide();
     this.el.insertAdjacentHTML('afterbegin', html.join(' '));
   };
 
@@ -26,7 +32,9 @@ function renderPost (post) {
     ? '<li class="tag tag-blue tag-rounded">Новость</li>'
     : '<li class="tag tag-rounded">Заметка</li>';
 
-  const button = '<button class ="button-round button-small button-primary">Сохранить</button>';
+  const button = (JSON.parse(localStorage.getItem('favorites')) || []).includes(post.id)
+  ? `<button class ="button-round button-small button-danger" data-id="${post.id}" data-title="${post.title}">Удалить</button>`
+  : `<button class ="button-round button-small button-primary" data-id="${post.id}" data-title="${post.title}">Сохранить</button>`
 
   return `
   <div class="panel">
@@ -45,4 +53,31 @@ function renderPost (post) {
     </div>
   </div>
 `
-}
+};
+
+function buttonHandler (evt) {
+  const el = evt.target;
+  const id = el.dataset.id;
+  const title = el.dataset.title;
+
+  if (id && title) {
+    let favotires = JSON.parse(localStorage.getItem('favorites')) || [];
+    console.log(favotires);
+
+    if (favotires.includes(id)) {
+      el.textContent = 'Сохранить';
+      el.classList.add('button-primary');
+      el.classList.remove('button-danger');
+      favotires = favotires.filter(fId => fId != id && fId != title);
+    } else {
+      el.textContent = 'Удалить';
+      el.classList.remove('button-primary');
+      el.classList.add('button-danger');
+      favotires.push(id, title);
+    }
+
+    localStorage.setItem('favorites', JSON.stringify(favotires));
+  }
+};
+
+// filter(fId => fId != id && fId != title);
--- source/index.js
+++ source/index.js
@@ -12,12 +12,14 @@ import { NavigationComponent } from './components/navigation.js';
 import { CreateComponent } from './components/create.js';
 import { FavoriteComponent } from './components/favorite.js';
 import { PostsComponent } from './components/posts.js';
+import { LoaderComponent } from './components/loader.js';
 
 new HeaderComponent('header');
 
 const navigation = new NavigationComponent('navigation');
+const loader = new LoaderComponent('loader');
 
-const posts = new PostsComponent('posts');
+const posts = new PostsComponent('posts', {loader: loader});
 const create = new CreateComponent('create');
 const favorite = new FavoriteComponent('favorite');
 
